30ae38a68d94ddb206c80122b06dc75e80f8abb0
baizhi958216 <1475289190@qq.com>
2023-09-24 22:19:27 +0800

    feat: 初始化bun vite ts react项目
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ src/Counter.tsx
@@ -0,0 +1,22 @@
+import { ReactNode, useState } from "react";
+
+type ChildrenType = {
+  children: (num: number) => ReactNode;
+};
+const Counter = ({ children }: ChildrenType) => {
+  const [count, setCount] = useState<number>(1);
+  const increment = () => setCount((prev) => prev + 1);
+  const decrement = () => setCount((prev) => prev - 1);
+
+  return (
+    <>
+      <h1>{children(count)}</h1>
+      <div>
+        <button onClick={decrement}>-</button>
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
+
+export default Counter;
